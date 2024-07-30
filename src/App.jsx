55897a113ed7a3f7db619f1fdcea154d87aa11d2
Alin Nita <mr.alin.nita@gmail.com>
2024-07-30 16:10:17 +0100

    logo change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,79 @@
 import "./App.css";
+import React, { useState } from "react";
+import { sites } from "./data";
 
-function App() {
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setResults(
+      sites.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <h1>Hello Alin</h1>
-      <h2>TEST</h2>
+    <div class="MainPage">
+      <div class="logo">
+        <img class="logo" src={require("./assets/evri-logo.png")} alt="logo" />
+      </div>
+      <form className="" onSubmit={handleSearch}>
+        <div className="search-box">
+          <input
+            className="input"
+            type="text"
+            placeholder="search sites"
+            value={searchTerm}
+            onChange={handleChange}
+          />
+        </div>
+        <br />
+        <button type="submit" className="buttonSearch">
+          Search
+        </button>
+        <div>
+          {results.map((item) => (
+            <div className="siteItems">
+              <div className="data">
+                <h2>{item.name}</h2>
+                <h3>{item.adress}</h3>
+                <br></br>
+                <h4>{item.routeInformations}</h4>
+                <h4>{item.description}</h4>
+                <h4>{item.maneouvring}</h4>
+                <h4>{item.potting}</h4>
+                <h4>{item.specialRequirements}</h4>
+                <h4>{item.generalInformations}</h4>
+                <h4>{item.notes}</h4>
+                <h4>
+                  {" "}
+                  - Real images with the actual site during delivery{" "}
+                  <span>&#8595;&#8595;&#8595;</span>
+                </h4>
+              </div>
+              <div className="container">
+                {item.image.map((x) => (
+                  <img className="image" src={require(`${x}`)} alt={x.image} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </form>
+      <div className="trademark">
+        <p>
+          Evri UK <span>&#174;</span> All Rights Reserved
+        </p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const sites = [
+  {
+    id: 1,
+    name: "Gwalia-Essar",
+    adress: " London Road, Holyhead LL65 3DR",
+    routeInformations: " - Route informations",
+    description:
+      " - Site description , e.g. commercial / retail, DCD / LCD, etc ",
+    maneouvring:
+      " - List of maneouvers to be performed in order to position the truck safely for the delivery once on site",
+    potting:
+      " - Informations regarding potting , e.g. super unleaded in front pots, DERV in the back, etc",
+    specialRequirements:
+      " - Special requirements: e.g. 22 foot long hose, grid lifter, etc",
+    generalInformations:
+      " - General informations : grids / inlets, potential hazards identified",
+    notes:
+      " - All these things to be filled by a competent person after properly assesing the site",
+    image: [
+      "./assets/pictures/Gwalia-Essar1.jpg",
+      "./assets/pictures/Gwalia-Essar2.jpg",
+      "./assets/pictures/Gwalia-Essar3.jpg",
+    ],
+  },
+  {
+    id: 2,
+    name: "Prysor-Essar",
+    adress: "Trawsfynydd, Blaenau Ffestiniog LL41 4TW",
+    routeInformations: " - Route informations",
+    description:
+      " - Site description , e.g. commercial / retail, DCD / LCD, etc ",
+    maneouvring:
+      " - List of maneouvers to be performed in order to position the truck safely for the delivery once on site",
+    potting:
+      " - Informations regarding potting , e.g. super unleaded in front pots, DERV in the back, etc",
+    specialRequirements:
+      " - Special requirements: e.g. 22 foot long hose, grid lifter, etc",
+    generalInformations:
+      " - General informations : grids / inlets, potential hazards identified",
+    notes:
+      " - All these things to be filled by a competent person after properly assesing the site",
+    image: [
+      "./assets/pictures/Prysor-Essar1.jpg",
+      "./assets/pictures/Prysor-Essar2.jpg",
+      "./assets/pictures/Prysor-Essar3.jpg",
+    ],
+  },
+];
